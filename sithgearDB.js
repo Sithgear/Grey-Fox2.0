aa85fc2047d8514a9b78e0e0da69446f478b04ca
Sithgear <Sithgear@gmail.com>
2018-03-19 21:24:25 -0600

    more db

--- sithgearDB.js
+++ sithgearDB.js
@@ -1,4 +1,3 @@
-
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://Sithgear:PzGzDb32!@ground-zeroes-shard-00-00-x5o2k.mongodb.net:27017,ground-zeroes-shard-00-01-x5o2k.mongodb.net:27017,ground-zeroes-shard-00-02-x5o2k.mongodb.net:27017/test?ssl=true&replicaSet=Ground-Zeroes-shard-0&authSource=admin');
